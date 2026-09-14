b30050ea536fe1d5f39c78efbb0d3b022340df09
Minsu Kang <bluehawhy@gmail.com>
2026-09-14 10:46:56 +0900

    feat: add test case descriptions for automation scripts

---
+++ src/automation/__init__.py
@@ -0,0 +1,8 @@
+'''
+테스트케이스 종류 정리할꺼임
+
+1. excel에서 "좌표값" 확인 후 좌표값 까지 스크롤 후 사진 찍기 - scroll_map_and_screenshot.py 
+2. excel에서 "시작점", "도착점" 확인 후 시작점에 자차 옮겨서 도착점까지 경로 탐색 - route_start_end.py
+3. excel에 "목적지" 정보 입력 후 검색 결과에 대한 스크린샷 찍기 - free_text_search_result.py
+4. excel에 "목적지" 정보 입력 후 해당 위치까지 경로 탐색 - route_destination.py
+'''
